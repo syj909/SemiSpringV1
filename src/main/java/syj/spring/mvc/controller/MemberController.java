dffa9966f153b6739fe7ebb7cd5da3a6d3fc26c0
syj909 <syj960909@gmail.com>
2022-09-29 10:05:02 +0900

    requestMapping & html 작성

--- src/main/java/syj/spring/mvc/controller/MemberController.java
+++ src/main/java/syj/spring/mvc/controller/MemberController.java
@@ -1,5 +1,23 @@
 package syj.spring.mvc.controller;
 
-public class MemberController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class MemberController {
+	
+	@GetMapping("/join")
+	public String join() {
+		return "join/join";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "join/login";
+	}
+	
+	@GetMapping("/myinfo")
+	public String myinfo() {
+		return "join/myinfo";
+	}
 }
